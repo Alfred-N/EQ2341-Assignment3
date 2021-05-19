58347bd3f9e613253afa08c1e286b2808e846d9b
Alfred-N <alfredn@kth.se>
2021-05-19 17:55:07 +0200

    stuff

--- Assignment5_BaumWelch.py
+++ Assignment5_BaumWelch.py
@@ -37,7 +37,8 @@ semi2 = np.concatenate((np.reshape(semi2,(len(semi2),1)),np.zeros([len(semi2),1]
 semi3 = np.concatenate((np.reshape(semi3,(len(semi3),1)),np.zeros([len(semi3),1])),axis=1)
 
 # Define a HMM
-n_states=2
+n_states=5
+end_state=0
 # q_old = np.array([0.8, 0.2])
 q = np.random.uniform(size=(n_states,))
 q[0]=1
@@ -57,7 +58,7 @@ for i in range(n_states):
 
 means = np.array([[i, 0.] for i in range(n_states)])
 
-covs = np.array([[[1, 0],[0, 10**(-3)]] for i in range(n_states)])
+covs = np.array([[[1, 0],[0, 10**(-2)]] for i in range(n_states)])
 
 # print(means)
 # # print(np.shape(covs_test))
@@ -79,6 +80,7 @@ np.set_printoptions(suppress=True)
 # # print(semi1[0:10])
 
 song_1 = np.array([semi1, semi2])
+song_1_2 = np.array([semi2])
 song_2 = np.array([semi3])
 
 # # print('True HMM parameters:')
@@ -97,20 +99,24 @@ song_2 = np.array([semi3])
 qstar = np.random.uniform(size=(n_states,))
 qstar = qstar/sum(qstar)
 
-Astar = np.random.uniform(size=(n_states,n_states))
-for i in range(n_states):
+Astar = np.random.uniform(size=(n_states-end_state,n_states))
+for i in range(n_states-end_state):
+    if i==0:
+        Astar[0,0]=1
+    else:
+        Astar[i,0]=0
     Astar[i,:] = Astar[i,:]/sum(Astar[i,:])
-
-meansstar = np.zeros([n_states,2])
-covsstar =  np.array([[[1, 0],[0, 10**(-3)]] for i in range(n_states)])
-Bstar = np.array([multigaussD(meansstar[i],covsstar[i]) for i in range(n_states)])
+print(Astar)
+meansstar = np.zeros([n_states-end_state,2])
+covsstar =  np.array([[[1, 0],[0, 10**(-0)]] for i in range(n_states-end_state)])
+Bstar = np.array([multigaussD(meansstar[i],covsstar[i]) for i in range(n_states-end_state)])
 
 hm_learn = HMM(qstar, Astar, Bstar)
 
 
 print("Running the Baum Welch Algorithm...")
-hm_learn.baum_welch(song_1, 20, prin=1, uselog=False)
-
+# hm_learn.baum_welch(song_1_2, 20, prin=1, uselog=False) #melody 2
+hm_learn.baum_welch(song_2, 20, prin=1, uselog=False) #melody 3
 
 
 # print("True States:\n",true_states[0:10] )
@@ -127,9 +133,9 @@ _, c3 = hm_learn.alphahat(semi3)
 log_likelihood1 = np.sum(np.log(c1))
 log_likelihood2 = np.sum(np.log(c2))
 log_likelihood3 = np.sum(np.log(c3))
-print("likelihood of song 1 = ", log_likelihood1)
-print("likelihood of song 2 = ", log_likelihood2)
-print("likelihood of song 3 = ", log_likelihood3)
+print("likelihood of melody 1 = ", log_likelihood1)
+print("likelihood of melody 2 = ", log_likelihood2)
+print("likelihood of melody 3 = ", log_likelihood3)
 
 
 
